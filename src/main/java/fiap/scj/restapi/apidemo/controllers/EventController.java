d4004b9d6ab0ad1d615d65aad619285bfbba0136
LeoSSFiap <111468080+LeoSSFiap@users.noreply.github.com>
2022-08-17 20:04:46 -0300

    Atualizando o nome da turma

--- src/main/java/fiap/scj/restapi/apidemo/controllers/EventController.java
+++ src/main/java/fiap/scj/restapi/apidemo/controllers/EventController.java
@@ -29,7 +29,7 @@ public class EventController {
     @GetMapping("/test")
     public String test() {
         // GET http://localhost:8080/event/test
-        return "<h1>Teste efetuado com sucesso. Parabéns a turma do 84AOJ!</h1>";
+        return "<h1>Teste efetuado com sucesso. Parabéns a turma do 87AOJ!</h1>";
     }
 
     // Retrieve All
